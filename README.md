dfdf54a83f0238033b1e57ab0af4f0c87fcec73c
R.Nanba <rnanba@gmail.com>
2024-12-30 13:36:00 +0900

    first release

---
+++ README.md
@@ -0,0 +1,99 @@
+# SER FFMPEG v0.1
+
+## 概要
+
+SER動画をフレーム指定で切り出して ffmpeg で一般的な動画形式に変換するスクリプトです。SER動画のタイムスタンプからタイムスタンプ表示を追加する機能もあります。
+
+ffmpeg は元々SER動画の入力に対応していますが、フレーム指定の切り出し(trim フィルター)の際に開始フレームが後ろの方にあると頭出しが非常に遅い(おそらく動画を先頭から読み込んでフレームを数えて頭出ししている)ので、SERの固定長フレームの性質を利用して高速に頭出しできるようにしました。
+
+また、RAWで撮影したカラーSER動画の debayer 処理をスクリプト側で行うので ffmpeg でフレームのクロッピング(crop フィルター)を行った際に色がおかしくなる(debayer がバグる？)問題も回避できます。
+
+## 動作環境
+
+以下の環境で動作を確認しています。
+
+- python 3.10.12
+  - numpy 2.2.0
+  - opencv-python 4.10.0.84
+  - pillow 11.0.0
+- ffmpeg 4.4.2
+
+venv 環境で `pip install -r requirments.txt` でモジュールをインストールして動作確認しています。
+
+OS は Ubuntu 22.04 で動作を確認しています。Windows, macOS でも動くように作ってありますが確認はしていません。以下の使用例では Linux の bash でのコマンドラインを例示しています。
+
+入力動画は SharpCap 4.1 で撮影した ASI294MC Pro の 8bit RAW 動画と ASI290MC の 16bit RAW 動画で動作を確認しています。
+
+タイムスタンプは SharpCap 等で撮影した際にSER動画内に埋め込まれたフレーム毎のタイムスタンプ値(SharpCap のオプションでフレーム画像にタイムスタンプを描画する機能とは別に常に数値として記録されているものです)を使用しています。
+
+## 使用方法
+
+```
+usage: ser-ffmpeg.py [-h] [--speed SPEED] [--ffplay] [--no-timestamp]
+                     [--timestamp-only] [--localtime] [--font FONT]
+                     [--font-size FONT_SIZE] [--font-color FONT_COLOR]
+                     [--timestamp-position TIMESTAMP_POSITION]
+                     [--timestamp-margin TIMESTAMP_MARGIN TIMESTAMP_MARGIN]
+                     ser_file start end framerate
+```
+
+ffmpeg には暗黙のうちに ser-ffmpeg が入力されたSER動画から切り出したフレームが1つ目の入力動画として指定されます。
+
+上で列挙された引数とオプション以外のオプション・引数は全て ffmpeg に渡されるので、`framerate` 以降に適切なオプションと出力ファイル名を指定することでSER動画を ffmpeg で可能な任意の加工を施し、任意の形式に変換して出力することができます。
+
+例:
+```
+./ser-ffmpeg.py foo.ser 123 456 29.97 -c:v libx264 -crf 17 test-h264.mp4
+```
+
+この例では、SER動画 `foo.ser` のフレーム番号123から456までを `29.97`fpsの動画として ffmpeg に入力し、ビデオコーデック libx264 でレート固定係数(Constant Rate Factor) 17 でMP4動画 test-h264.mp4 に出力します。
+
+###  引数
+
+| 引数        | 説明                                              |
+|-------------|---------------------------------------------------|
+| `ser_file`  | 入力するSER動画を指定します。                     |
+| `start`     | 処理対象部分の開始フレーム番号を指定します。      |
+| `end`       | 処理対象部分の終了フレーム番号を指定します。      |
+| `framerate` | 入力するSER動画の平均フレームレートを指定します。 |
+
+フレーム番号は [https://github.com/cgarry/ser-player](SERPlayer) が表示するものと同じで1から始まる番号です。平均フレームレートも SERPlayer に表示されるものを指定すれば OK です。
+
+### オプション
+| オプション                             | 説明                                                                                                                                      | デフォルト値 |
+|----------------------------------------|-------------------------------------------------------------------------------------------------------------------------------------------|--------------|
+| `--speed SPEED`                        | ffmpeg に入力する映像を何倍速にするか(何倍速相当でフレームをスキップするか)を指定します。                                                 | `1`          |
+| `--ffplay`                             | ffmpeg ではなく ffplay に映像を ffmpeg に入力します。ffmpeg に指定するフィルターの効果の確認等に使います。                                |              |
+| `--no-timestamp`                       | タイムスタンプを表示しない映像を ffmpeg に入力します(後述)。                                                                              |              |
+| `--timestamp-only`                     | 黒バックにタイムスタンプのみを表示した映像を ffmpeg に入力します(後述)。                                                                  |              |
+| `--font FONT`                          | タイムスタンプ表示のフォントを指定します。指定がなければ環境から推定した Courier New フォントを使用します(後述)。                         |              |
+| `--font-size FONT_SIZE`                | タイムスタンプ表示のフォントサイズを指定します(単位はピクセル)。                                                                          | `24`         |
+| `--font-color FONT_COLOR`              | タイムスタンプ表示の文字色を指定します。HTML/CSSのカラーコードが使えます。                                                                | `#FF8800C0`  |
+| `--timestamp-position TEXT_POSITION`   | タイムスタンプの表示位置を `top-left`, `top-middle`, `top-right`, `bottom-left`, `bottom-middle`, `bottom-right` のいずれかで指定します。 | `top-left`   |
+| `--timestamp-margin MARGIN_X MARGIN_Y` | タイムスタンプ表示のX方向とY方向のマージンを指定します(単位はピクセル)。                                                                  | `0` `0`      |
+
+### フォントについて
+
+デフォルトでは Windows, Linux(Ubuntu 等のディストリビューション), macOS がシステムに標準で持つ Courier New フォントを使用します。他の OS では明示的に `--font FONT` オプションを指定しないとエラーになります。`FONT` に指定するのは TrueType フォントのフォントファイル名です。詳細は[Pillow の ImageFont の説明](https://pillow.readthedocs.io/en/stable/reference/ImageFont.html)を参照してください。
+
+## ライセンス
+
+MITライセンスです。
+
+## 参考: タイムスタンプと映像の分離処理
+
+`--timestamp-only` オプションと `--no-timestamp` オプションでタイムスタンプのみの動画とタイムスタンプなしの動画を作成し、それらを合成することで、タイムスタンプの色調を変えずに元の映像のみに適切なフィルター処理を適用した動画を作成することができます。
+
+例:
+```
+# タイムスタンプなし動画の作成。ノイズ低減、明度UP、低圧縮の H264 動画。
+./ser-ffmpeg.py --no-timestamp 18_05_00.ser 22870 31855 30 -vf 'hqdn3d,eq=gamma=3.13:contrast=1.1155:brightness=-0.01535' -c:v libx264 -crf 18 -pix_fmt yuv444p 2024-12-8-18_05_00-22870_31855-no-timestamp.mp4
+# タイムスタンプのみ動画(アルファチャンネル付き)の作成。低圧縮の vp9/webm 動画。
+./ser-ffmpeg.py --timestamp-only 18_05_00.ser 22870 31855 30 -c:v vp9 -crf 18 2024-12-8-18_05_00-22870_31855-timestamp-only.webm
+# タイムスタンプなし動画とタイムスタンプなし動画をアルファブレンディングで合成。
+ffmpeg -i 2024-12-8-18_05_00-22870_31855-no-timestamp.mp4 -c:v libvpx-vp9 -i 2024-12-8-18_05_00-22870_31855-timestamp-only.webm -filter_complex "[0][1]overlay" -c:v libx264 -b:v 10M -pix_fmt yuv420p 2024-12-8-18_05_00-22870_31855-with-timestamp.mp4
+```
+
+タイムスタンプのみ動画を作成する際は ffmpeg のオプションに `-c:v vp9` を、出力ファイル名の拡張子に .webm を指定してアルファチャンネル対応の動画形式で出力します。
+
+ffmpeg でタイムスタンプのみ動画とタイムスタンプなし動画を合成する際には `-c libvpx-vp9` オプションをタイムスタンプのみの動画(.webm 形式)を指定する `-i` オプションの直前に指定します。これを指定しないとアルファチャンネルが正しく処理されません。
---
+++ ser-ffmpeg.py
@@ -0,0 +1,293 @@
+#!/usr/bin/env python
+# SER ファイルをフレームカウント指定で切り出して ffmpeg でエンコードします。
+# 事前に ffmpeg をインストールしてパスを通しておく必要があります。
+# 例: ser-ffmpeg.py foo.ser 123 456 29.97 -c:v libx264 -crf 17 test-h264.mp4
+# 5つ目以降のコマンドライン引数は ffmpeg にそのまま渡されます。
+
+import argparse
+import os
+import sys
+import subprocess
+import traceback
+import platform
+import datetime
+import numpy as np
+import cv2
+from PIL import Image, ImageDraw, ImageFont
+
+# Only for RAW8 or RAW16 file of bayer color camera.
+class SerVideo:
+    EPOCH = datetime.datetime(1, 1, 1, tzinfo=datetime.timezone.utc)
+    BAYER_PATTERNS = {
+        8: cv2.COLOR_BAYER_RGGB2RGB,
+        9: cv2.COLOR_BAYER_GRBG2RGB,
+        10: cv2.COLOR_BAYER_GBRG2RGB,
+        11: cv2.COLOR_BAYER_BGGR2RGB
+    }
+    def read_int(self, f, n):
+        return int.from_bytes(f.read(n), 'little')
+    def int_to_timestamp(self, t8):
+        return self.EPOCH + datetime.timedelta(microseconds=t8/10)
+    def read_timestamp(self, f):
+        return self.int_to_timestamp(self.read_int(f, 8))
+
+    def __init__(self, input):
+        f = open(input, "rb")
+        file_id = f.read(14).decode(encoding='utf-8')
+        lu_id = self.read_int(f, 4)
+        self.color_id = self.read_int(f, 4)
+        self.little_endian = self.read_int(f, 4)
+        self.image_width = self.read_int(f, 4)
+        self.image_height = self.read_int(f, 4)
+        self.pixel_depth = self.read_int(f, 4)
+        self.frame_count = self.read_int(f, 4)
+        self.observer = f.read(40).decode(encoding='utf-8')
+        self.instrume = f.read(40).decode(encoding='utf-8')
+        self.telescope = f.read(40).decode(encoding='utf-8')
+        self.date_time = self.read_int(f, 8)
+        self.date_time_utc = self.read_int(f, 8)
+
+        if self.date_time == 0:
+            raise RuntimeError(f"SER file '{input}' has no frame timestamps.")
+
+        self.frame_length = \
+            self.image_width * self.image_height * self.pixel_depth//8
+        if self.pixel_depth == 8:
+            self.dtype = np.dtype('u1')
+        elif self.pixel_depth == 16:
+            self.dtype = np.dtype('u2')
+        else:
+            raise RuntimeError(f"Unsupported pixel depth: {self.pixel_depth}")
+
+        if self.little_endian == 0:
+            self.dtype = self.dtype.newbyteorder('>')
+        else:
+            self.dtype = self.dtype.newbyteorder('<')
+        
+        self.timestamps = []
+        f.seek(178 + self.frame_count * self.frame_length)
+        for p in range(self.frame_count):
+            self.timestamps.append(self.read_timestamp(f))
+        self.f = f
+
+    def timestamp_of_frame_number(self, frame_number):
+        return self.timestamps[frame_number - 1]
+
+    def image_of_frame_number(self, frame_number):
+        self.f.seek(178 + (frame_number - 1) * self.frame_length)
+        array = np.frombuffer(self.f.read(self.frame_length), dtype=self.dtype)
+        image_array = np.reshape(array, (self.image_height, self.image_width))
+        cv2_image = cv2.cvtColor(image_array, self.BAYER_PATTERNS[self.color_id])
+        return cv2_image
+
+    def close(self):
+        self.f.close()
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self):
+        self.close()
+
+FONTS = {
+    "Linux": "Courier_New.ttf",
+    "Windows": "cour.ttf",
+    "Darwin": "Courier.ttc"
+}
+TEXT_ANCHORS = {
+    "top-left": "la",
+    "top-middle": "ma",
+    "top-right": "ra",
+    "bottom-left": "ld",
+    "bottom-middle": "md",
+    "bottom-right": "rd"
+}
+
+LOCAL_TZ = datetime.datetime.now().astimezone().tzinfo
+
+def get_font(font_filename, font_size):
+    if font_filename is None:
+        if platform.system() not in FONTS:
+            abort("ERROR: Default font of OS is not detected."\
+                  " Please specify font filename with --font.")
+        else:
+            font_filename = FONTS[platform.system()]
+    
+    return ImageFont.truetype(font_filename, font_size)
+
+def test_text_position(pos):
+    if pos not in TEXT_ANCHORS:
+        pos_keys = None
+        for pos_key in TEXT_ANCHORS.keys():
+            if pos_keys is None:
+                pos_keys = ""
+            else:
+                pos_keys += ", "
+            pos_keys += f"'{pos_key}'"
+        abort(f"ERROR: Unknown text position: '{pos}'. Specify {pos_keys}.")
+    return True
+    
+def get_text_position(pos, width, height, margin_x, margin_y):
+    if pos =="top-left":
+        return (margin_x, margin_y)
+    elif pos == "top-middle":
+        return (width/2, 0 + margin_y)
+    elif pos == "top-right":
+        return (width - margin_x, margin_y)
+    elif pos == "bottom-left":
+        return (margin_x, height - margin_y)
+    elif pos == "bottom-middle":
+        return (width/2, height - margin_y)
+    elif pos == "bottom-right":
+        return (width - margin_x , height - margin_y)
+    else:
+        return None
+
+def draw_timestamp(draw, t, pos, anchor, font, color):
+    t_str = f"{str(t)}"
+    draw.text(pos, f"{t_str}", fill=color, font=font, anchor=anchor)
+
+def scale_to_16bit(canvas_16, channel_index):
+    return canvas_16[..., channel_index] * 257
+
+def split_channel(image_16, channel_index):
+    return image_16[..., channel_index]
+
+def blend_channel(fg, alpha, bg):
+    return (alpha/65535 * fg + (65535 - alpha)/65535 * bg).astype(np.uint16)
+
+def alpha_blending(canvas_8, image):
+    canvas_16 = np.array(canvas_8, dtype=np.uint16)
+    canvas_r = scale_to_16bit(canvas_16, 0)
+    canvas_g = scale_to_16bit(canvas_16, 1)
+    canvas_b = scale_to_16bit(canvas_16, 2)
+    canvas_a = scale_to_16bit(canvas_16, 3)
+    image_r = split_channel(image, 0)
+    image_g = split_channel(image, 1)
+    image_b = split_channel(image, 2)
+    image_r = blend_channel(canvas_r, canvas_a, image_r)
+    image_g = blend_channel(canvas_g, canvas_a, image_g)
+    image_b = blend_channel(canvas_b, canvas_a, image_b)
+    return np.stack((image_r, image_g, image_b), axis=-1)
+
+parser = argparse.ArgumentParser()
+parser.add_argument("ser_file", 
+                    help="SER file.")
+parser.add_argument("start", type=int,
+                    help="start frame count.")
+parser.add_argument("end", type=int,
+                    help="end frame count.")
+parser.add_argument("framerate",
+                    help="framerate of SER.")
+parser.add_argument("--speed", type=int, default=1,
+                    help="skip frames for Nx speed.")
+parser.add_argument("--ffplay", action="store_true",
+                    help="exec ffplay instead of ffmpeg.")
+
+parser.add_argument("--no-timestamp", action="store_true",
+                    help="do not draw timestamps")
+parser.add_argument("--timestamp-only", action="store_true",
+                    help="output frames with only timestamps drawn.")
+parser.add_argument("--localtime", action="store_true",
+                    help="draw timestamps in local time.")
+parser.add_argument("--font", default=None,
+                    help="Font filename of timestamps.")
+parser.add_argument("--font-size", type=int, default=24,
+                    help="Font size (pixels) of timestamps.")
+parser.add_argument("--font-color", default="#FF8800C0",
+                    help="Font color of timestamps.")
+parser.add_argument("--timestamp-position", default="top-left",
+                    help="Position of timestamps. The options 'top-left',"\
+                    " 'top-middle', 'top-right', 'bottom-left', 'bottom-middle',"\
+                    " and 'bottom-right' can be specified.")
+parser.add_argument("--timestamp-margin", nargs=2, type=int, default=[0,0],
+                    help="x and y margin width (pixels) of timestamps.")
+args, ffmpeg_args = parser.parse_known_args()
+
+if args.no_timestamp and args.timestamp_only:
+    abort("ERROR: --no-timestamp and --timestamp-only are exclusive options.")
+
+ser = SerVideo(args.ser_file)
+pix_fmt = None
+if args.timestamp_only:
+    pix_fmt = "rgba"
+else:
+    if ser.pixel_depth == 8:
+        pix_fmt = "rgb24"
+    elif ser.pixel_depth == 16:
+        pix_fmt = "rgb48"
+
+if pix_fmt is None:
+    sys.exit(f"ERROR: unsupported pixel depth: {ser.pixel_depth}")
+if not ser.color_id in ser.BAYER_PATTERNS:
+    sys.exit(f"ERROR: unsupported SER color id: {ser.color_id}")
+if args.start <= 0 or ser.frame_count < args.start:
+    sys.exit(f"ERROR: invalid `start` value "\
+             f"(valid range: 1-{ser.framecount}).")
+if ser.frame_count < args.end or args.end < args.start:
+    sys.exit(f"ERROR: invalid `end` value "\
+             f"(valid range: {args.start}-{ser.frame_count}).")
+
+font = None
+font_color = None
+text_pos = None
+text_anchor = None
+if not args.no_timestamp:
+    test_text_position(args.timestamp_position)
+    text_anchor = TEXT_ANCHORS[args.timestamp_position]
+    text_pos = get_text_position(args.timestamp_position,
+                                 ser.image_width,
+                                 ser.image_height,
+                                 args.timestamp_margin[0],
+                                 args.timestamp_margin[1])
+    font = get_font(args.font, args.font_size)
+    font_color = args.font_color
+
+ffmpeg = "ffmpeg"
+if args.ffplay:
+    ffmpeg = "ffplay"
+ffmpeg_cmd = [ffmpeg,
+              "-f", "rawvideo",
+              "-pixel_format", pix_fmt,
+              "-s", f"{ser.image_width}x{ser.image_height}",
+              "-framerate", args.framerate,
+              "-i", "-"]
+ffmpeg_cmd.extend(ffmpeg_args)
+ffmpeg_process = subprocess.Popen(ffmpeg_cmd,
+                                  stdin=subprocess.PIPE,
+                                  stdout=sys.stdout,
+                                  stderr=sys.stderr)
+try:
+    for i in range(args.start, args.end + 1):
+        if (i - args.start) % args.speed != 0:
+            continue
+        image = None
+        if args.no_timestamp:
+            image = ser.image_of_frame_number(i)
+        else:
+            if pix_fmt == "rgb24":
+                canvas = Image.fromarray(ser.image_of_frame_number(i))
+            else:
+                canvas = Image.new(mode="RGBA", color="#00000000",
+                                   size=(ser.image_width, ser.image_height))
+            draw = ImageDraw.Draw(canvas)
+            t = ser.timestamp_of_frame_number(i)
+            if args.localtime:
+                t = t.astimezone(LOCAL_TZ)
+            draw_timestamp(draw, t, text_pos, text_anchor, font, font_color)
+            if pix_fmt == "rgb48":
+                image = alpha_blending(canvas, ser.image_of_frame_number(i))
+            else:
+                image = np.array(canvas)
+        
+        ffmpeg_process.stdin.write(image.tobytes())
+    
+except Exception as e:
+    print(f"ERROR: {e}")
+    print(traceback.format_exc())
+finally:
+    ser.close()
+    if ffmpeg_process.poll() is None:
+        ffmpeg_process.stdin.close()
+        ffmpeg_process.wait()
+        ffmpeg_process.terminate()
